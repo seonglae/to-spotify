955f3ade18cdeb6022e9b6ffc635a33e7cae693d
seonglae <sungle3737@gmail.com>
2021-06-24 22:55:51 +0900

    refactor: implements musicface

--- src/worker/m2s.ts
+++ src/worker/m2s.ts
@@ -1,46 +1,48 @@
 import Migrator from '../core/migrator'
+import Musicface from '../core/musicface'
 
 const MELON = 'https://www.melon.com'
 const FILTER = ['#']
 const TITLE_QUERY = '.more_txt_title'
 const NEXT_QUERY = '.btn_next'
 
-export class M2S extends Migrator {
+export class M2S implements Musicface {
   mkey?: string
   pseq?: string
+  migrator: Migrator
 
   constructor(stoken: string, options: { mkey?: string; pseq?: string }) {
-    super(stoken, { nextQuery: NEXT_QUERY })
+    this.migrator = new Migrator(stoken, { nextQuery: NEXT_QUERY })
     this.mkey = options.mkey
     this.pseq = options.pseq
   }
 
-  public async melonPlaylist(name?: string, open?: boolean) {
+  public async playlist(name?: string, open?: boolean) {
     const url = `${MELON}/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=${this.pseq}`
     const queries = ['.wrapArtistName div a', '.wrap .ellipsis a span']
     const filters = queries.map(() => FILTER)
-    super.playlist(url, queries, filters, TITLE_QUERY, name, open)
+    this.migrator.playlist(url, queries, filters, TITLE_QUERY, name, open)
   }
 
-  public async likedMelonArtists(): Promise<void> {
+  public async likedArtists(): Promise<void> {
     const url = `${MELON}/mymusic/like/mymusiclikesong_list.htm?memberKey=${this.mkey}`
     const queries = ['.artist-name']
     const filters = queries.map(() => FILTER)
-    super.likedArtists(url, queries, filters)
+    this.migrator.likedArtists(url, queries, filters)
   }
 
-  public async likedMelonAlbums(): Promise<void> {
+  public async likedAlbums(): Promise<void> {
     const url = `${MELON}/mymusic/like/mymusiclikealbum_list.htm?memberKey=${this.mkey}`
     const queries = ['dt.ellipsis', '.album-artist']
     const filters = queries.map(() => FILTER)
-    super.likedAlbums(url, queries, filters)
+    this.migrator.likedAlbums(url, queries, filters)
   }
 
-  public async likedMelonTracks(): Promise<void> {
+  public async likedTracks(): Promise<void> {
     const url = `${MELON}/mymusic/artistfan/mymusicartistfan_list.htm?memberKey=${this.mkey}`
     const queries = ['a.artist', 'a.title']
     const filters = queries.map(() => FILTER)
-    super.likedTracks(url, queries, filters)
+    this.migrator.likedTracks(url, queries, filters)
   }
 }
 
